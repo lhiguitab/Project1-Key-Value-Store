1abe2fde19b9763f9308e9642ff3e6c511fc4724
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-02-22 20:43:39 -0500

    Fix - Parsing the data

--- main.c
+++ main.c
@@ -4,12 +4,11 @@
 #include <time.h>
 
 #include "uthash.h"
-// #include "hash_table.h"
 
 #define MAX_LINE 4096
 
 void clear_cache() {
-    system("sudo su -c \"echo 3 > /proc/sys/vm/drop_caches\" > /dev/null 2>&1");
+    int result = system("sudo su -c \"echo 3 > /proc/sys/vm/drop_caches\" > /dev/null 2>&1");
 }
 
 // Define the structure of the CSV file
@@ -40,7 +39,7 @@ typedef struct {
     UT_hash_handle hh;  // Required for uthash
 } Review;
 
-// Declare games as a global variable
+// Declare the hash tables as a global variables
 Game *games = NULL;  // Hash table (key: app_id)
 User *users = NULL;  // Hash table (key: user_id)
 Review *reviews = NULL;  // Hash table (key: review_id)
@@ -95,6 +94,13 @@ void print_games() {
     }
 }
 
+void print_users(){
+    User *user, *tmp;
+    HASH_ITER(hh, users, user, tmp) {
+        printf("User: %d - %d\n", user->user_id, user->reviews);
+    }
+}
+
 void free_games() {
     Game *game, *tmp;
     HASH_ITER(hh, games, game, tmp) {
@@ -126,16 +132,41 @@ void load_games_from_csv(const char *filename) {
         return;
     }
 
+    printf("Loading games from CSV file...\n");
+    
     char line[MAX_LINE];  // Buffer for each line
+    char* temp = strcmp(filename, "./Dataset/games.csv") ;
     while (fgets(line, sizeof(line), file)) {
         int app_id, positive_ratio, user_reviews;
         char title[100], rating[20];
 
         // Parse CSV line (assuming format: app_id,title,rating,positive_ratio,user_reviews)
-        if (sscanf(line, "%d,%99[^,],%19[^,],%d,%d", &app_id, title, rating, &positive_ratio, &user_reviews) == 5) {
-            add_game(app_id, title, rating, positive_ratio, user_reviews);
+        char *token = strtok(line, ","); // app_id
+        if (token) {
+            app_id = atoi(token);
+            token = strtok(NULL, ","); // title
+            if (token) {
+                strncpy(title, token, sizeof(title));
+                for (int i = 0; i < 4; i++) {
+                    token = strtok(NULL, ","); //date, win, mac, linux
+                }
+                token = strtok(NULL, ","); // rating
+                if (token) {
+                    strncpy(rating, token, sizeof(rating));
+                    token = strtok(NULL, ","); // positive_ratio
+                    if (token) {
+                        positive_ratio = atoi(token);
+                        token = strtok(NULL, ","); // user_reviews
+                        if (token) {
+                            user_reviews = atoi(token);
+                            add_game(app_id, title, rating, positive_ratio, user_reviews);
+                        }
+                    }
+                }
+            }
         }
     }
+
     fclose(file);
 }
 
@@ -147,12 +178,20 @@ void load_users_from_csv(const char *filename) {
     }
 
     char line[MAX_LINE];  // Buffer for each line
+
     while (fgets(line, sizeof(line), file)) {
         int user_id, reviews;
 
         // Parse CSV line (assuming format: user_id,reviews)
-        if (sscanf(line, "%d,%d", &user_id, &reviews) == 2) {
-            add_user(user_id, reviews);
+        char *token = strtok(line, ","); // user_id
+        if (token) {
+            user_id = atoi(token);
+            token = strtok(NULL, ","); // products
+            token = strtok(NULL, ","); // reviews
+            if (token) {
+                reviews = atoi(token);
+                add_user(user_id, reviews);
+            }
         }
     }
     fclose(file);
@@ -170,8 +209,20 @@ void load_recommendations_from_csv(const char *filename) {
         int review_id, app_id, user_id;
 
         // Parse CSV line (assuming format: review_id,app_id,user_id)
-        if (sscanf(line, "%d,%d,%d", &review_id, &app_id, &user_id) == 3) {
-            add_review(review_id, app_id, user_id);
+        char *token = strtok(line, ","); // app_id
+        
+        if (token) {
+            app_id = atoi(token);
+            for (int i = 0; i < 5; i++) token = strtok(NULL, ","); // helpful, funny, date, is_recommended, hours
+            token = strtok(NULL, ","); // user_id
+            if (token) {
+                user_id = atoi(token);
+                token = strtok(NULL, ","); // review_id
+                if (token) {
+                    review_id = atoi(token);
+                    add_review(review_id, app_id, user_id);
+                }
+            }
         }
     }
     fclose(file);
@@ -181,11 +232,11 @@ int main() {
     // Clear the cache
     clear_cache();
     
-    CSVFile csv_files[3] = {
-        {"./Dataset/games.csv", 0},
-        {"./Dataset/players.csv", 0},
-        {"./Dataset/reviews.csv", 0}
-    };
+    // CSVFile csv_files[3] = {
+    //     {"./Dataset/games.csv", 0},
+    //     {"./Dataset/players.csv", 0},
+    //     {"./Dataset/reviews.csv", 0}
+    // };
     
     // Game *games = NULL;  // Hash table (key: app_id)
     // User *users = NULL;  // Hash table (key: user_id)
@@ -196,20 +247,22 @@ int main() {
 
     clock_t start = clock();
 
-    load_recommendations_from_csv("./Dataset/recommendations.csv");
-    // load_games_from_csv("./Dataset/games.csv");
+    // load_recommendations_from_csv("./Dataset/recommendations.csv");
+    load_games_from_csv("./Dataset/games.csv");
     // load_users_from_csv("./Dataset/users.csv");
 
     clock_t end = clock();
 
     clock_t start_search = clock();
-    find_review(1938090);
+    printf("app_id: %d, title: %s, rating: %s, positive_ratio: %d, user_reviews: %d\n", find_game(13500)->app_id, find_game(13500)->title, find_game(13500)->rating, find_game(13500)->positive_ratio, find_game(13500)->user_reviews);
     clock_t end_search = clock();
 
-    free_reviews();
+
+    // free_reviews();
 
     double time = (double)(end - start) / CLOCKS_PER_SEC;
     double time_search = (double)(end_search - start_search) / CLOCKS_PER_SEC;
+    
     printf("Execution time: %f seconds\n", time);
     printf("Search time: %f seconds\n", time_search);
 

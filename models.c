3f61e8c08d067e7502175349f6831d4255a2b9fd
SantiGomez2519 <sgomezo14@eafit.edu.co>
2025-02-28 01:00:27 -0500

    Update - File handling

---
+++ models.c
@@ -0,0 +1,51 @@
+#include "models.h"
+#include <stdlib.h>
+#include <string.h>
+
+/* Definición de variables globales */
+Game *games = NULL;
+User *users = NULL;
+Review *reviews = NULL;
+
+void add_game(int app_id, const char *title, const char *rating, int positive_ratio, int user_reviews) {
+    Game *game = malloc(sizeof(Game));
+    game->app_id = app_id;
+    strncpy(game->title, title, MAX_TITLE);
+    strncpy(game->rating, rating, MAX_RATING);
+    game->positive_ratio = positive_ratio;
+    game->user_reviews = user_reviews;
+    HASH_ADD_INT(games, app_id, game);
+}
+
+void add_user(int user_id, int reviews_count) {
+    User *user = malloc(sizeof(User));
+    user->user_id = user_id;
+    user->reviews = reviews_count;
+    HASH_ADD_INT(users, user_id, user);
+}
+
+void add_review(int review_id, int app_id, int user_id) {
+    Review *review = malloc(sizeof(Review));
+    review->review_id = review_id;
+    review->app_id = app_id;
+    review->user_id = user_id;
+    HASH_ADD_INT(reviews, review_id, review);
+}
+
+Game *find_game(int app_id) {
+    Game *game = NULL;
+    HASH_FIND_INT(games, &app_id, game);
+    return game;
+}
+
+User *find_user(int user_id) {
+    User *user = NULL;
+    HASH_FIND_INT(users, &user_id, user);
+    return user;
+}
+
+Review *find_review(int review_id) {
+    Review *review = NULL;
+    HASH_FIND_INT(reviews, &review_id, review);
+    return review;
+}
---
+++ store.h
@@ -0,0 +1,8 @@
+#ifndef STORE_H
+#define STORE_H
+
+#include "models.h"
+#include "csv_loader.h"
+#include "top_calculations.h"
+
+#endif /* STORE_H */
